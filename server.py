5bc612bd7675c74e888510a9c82ea946c82500f3
Alexis Tamayo <lexx426@gmail.com>
2022-12-17 15:29:53 -0600

    creation of the environmental variable of my API key, utilized the movie database API with my controller for the home page. Also fixed some css in two of my html pages

--- server.py
+++ server.py
@@ -1,7 +1,12 @@
 from flask_app import app
 from flask_app.controllers import user_controller
+import os
 
-
+from dotenv import load_dotenv
+# load_dotenv()
+# this is for creating an environmental variable , which is the API key in the .env file
 
 if __name__ == "__main__":   
+    load_dotenv()
     app.run(debug=True) 
+    # print(os.environ['API_KEY'])
